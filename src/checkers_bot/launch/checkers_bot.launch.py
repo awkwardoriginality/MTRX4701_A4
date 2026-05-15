55ba4d26cae40123e87bd49306310291d17a8552
Has <harrysalvesen@gmail.com>
2026-05-15 16:00:52 +1000

    Fix missing launch configuration import

--- src/checkers_bot/launch/checkers_bot.launch.py
+++ src/checkers_bot/launch/checkers_bot.launch.py
@@ -13,6 +13,7 @@ Usage:
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import os
 from ament_index_python.packages import get_package_share_directory
 
